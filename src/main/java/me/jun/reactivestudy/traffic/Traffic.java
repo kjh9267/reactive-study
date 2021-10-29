1ab8bf9c331288729827165631152893ddb0ae9d
kjh9267 <intellijunho@kakao.com>
2021-10-29 20:08:51 +0900

    feat: Create Async Controller

---
+++ src/main/java/me/jun/reactivestudy/traffic/Traffic.java
@@ -0,0 +1,39 @@
+package me.jun.reactivestudy.traffic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Traffic {
+
+    private static final String URL = "http://localhost:8080/async";
+
+    public static void main(String[] args) throws InterruptedException {
+        long totalStart = System.currentTimeMillis();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int count = 0; count < 1_000; count++) {
+            executorService.execute(
+                    () -> {
+                        long start = System.currentTimeMillis();
+                        restTemplate.getForObject(URL, String.class);
+                        long end = System.currentTimeMillis();
+                        log.info(String.valueOf(end - start));
+                    }
+            );
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(100, TimeUnit.SECONDS);
+
+        long totalEnd = System.currentTimeMillis();
+        log.info(String.valueOf((totalEnd - totalStart)));
+    }
+}
